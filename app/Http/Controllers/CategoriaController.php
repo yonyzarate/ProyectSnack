999f2052b888e68d93414ef3adf550f11b75f0bb
salvador_zarate <yony.zarate96@gmail.com>
2021-02-16 14:14:19 -0400

    controlador de categoria

---
+++ app/Http/Controllers/CategoriaController.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use app\Models\Categoria;
+use Illuminate\Support\Facades\Redirect;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Contracts\Container\BindingResolutionException;
+use Symfony\Component\VarDumper\Caster\RedisCaster;
+
+class CategoriaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+        if ($request) {
+            $sql = trim($request->get('buscarTexto'));
+            $categoria = DB::table('categoria')->where('Nombre','LIKE','%'.$sql.'%')->orderBy('IdCategoria','desc')->paginate(3);
+            // return view('categoria.index',["categoria"=>$categoria,"buscarTexto"=>$sql]);
+            return $categoria;
+        }
+    }
+
+    
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $categoria = new Categoria();
+        $categoria->Nombre= $request->nombre;
+        $categoria->Descripcion= $request->descripcion;
+        $categoria->Condicion= '1';
+        $categoria->save();
+        
+        return redirect::to('Categoria');
+
+    }
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request)
+    {
+        $categoria = Categoria::findOrFail($request->id_categoria);
+        $categoria->Nombre= $request->nombre;
+        $categoria->Descripcion= $request->descripcion;
+        $categoria->Condicion= '1';
+        $categoria->save();
+        return redirect::to('Categoria');
+
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Request $request)
+    {
+        //
+        $categoria = Categoria::findOrFail($request->id_categoria);
+
+        if($categoria->Condicion=='1'){
+            
+            $categoria->Condicion='0';
+            $categoria->save();
+            return redirect::to('Categoria');
+        }else{
+            $categoria->Condicion='1';
+            $categoria->save();
+            return redirect::to('Categoria');
+        }
+
+    }
+}
