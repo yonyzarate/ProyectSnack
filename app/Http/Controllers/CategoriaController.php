d9d6a59c9413cbbac273cf8f71f9952f95242370
salvador_zarate <yony.zarate96@gmail.com>
2021-04-02 04:08:11 -0400

    paginador de bootstrap

--- app/Http/Controllers/CategoriaController.php
+++ app/Http/Controllers/CategoriaController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use app\Models\Categoria;
+use App\Models\Categoria;
 use exception;
 use Illuminate\Support\Facades\Redirect;
 use Illuminate\Support\Facades\DB;
--- app/Http/Controllers/PDF_Controller.php
+++ app/Http/Controllers/PDF_Controller.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use app\Models\Categoria;
+use App\Models\Categoria;
 use Dompdf\Dompdf;
 use Barryvdh\DomPDF\Facade as  PDF;
 
